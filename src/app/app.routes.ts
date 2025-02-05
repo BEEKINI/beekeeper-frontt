414f5a206c093b6482c5f2cc29b08f9e488e1a0a
valerian <94058053+valerianchar@users.noreply.github.com>
2025-02-05 16:56:26 +0100

    Page de connexion (#26)
    * Page de connexion sans connexion
    ---------
    
    Co-authored-by: Valerian <vcharrier@optimis.io>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,20 @@
 import { Routes } from '@angular/router';
+import { ConnexionComponent } from './views/connexion/connexion.component';
 import { InscriptionComponent } from './views/inscription/inscription.component';
 export const routes: Routes = [
-  {
-    path: 'inscription',
-    component: InscriptionComponent,
-    pathMatch: 'full',
-  },
-  {
+    {
+        path: 'inscription',
+        component: InscriptionComponent,
+        pathMatch: 'full',
+    },
+    {
+        path : 'connexion',
+        component : ConnexionComponent,
+        pathMatch: 'full',
+    },
+    {
     path: '',
-    redirectTo: 'inscription',
+    redirectTo: 'connexion',
     pathMatch: 'full',
-  },
-];
+}];
+
