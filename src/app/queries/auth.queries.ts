ba008ecf4cc11e7cffb8e69e923357b616d493a6
Arno Peyronon <arno.peyronon@astree-software.fr>
2025-02-06 15:05:00 +0100

    liaison back connexion inscription

--- src/app/queries/auth.queries.ts
+++ src/app/queries/auth.queries.ts
@@ -3,19 +3,25 @@ import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../services/user.service';
 
+interface UserRegister extends User {
+  password_confirmation: string;
+}
+
+interface UserLogin {
+  email: string;
+  password: string;
+}
+
 @Injectable({ providedIn: 'root' })
 export class AuthQueries {
-  protected readonly baseUrl = 'http://localhost:80/';
+  protected readonly baseUrl = 'http://127.0.0.1/api';
   protected readonly http = inject(HttpClient);
 
-  public register(user: User): Observable<any> {
-    return this.http.post<any>(`${this.baseUrl}/register`, user);
+  public register(user: UserRegister): Observable<User> {
+    return this.http.post<User>(`${this.baseUrl}/register`, user);
   }
 
-  public login(email: string, password: string): Observable<any> {
-    return this.http.post<any>(`${this.baseUrl}/login`, {
-      email,
-      password,
-    });
+  public login(user: UserLogin): Observable<User> {
+    return this.http.post<User>(`${this.baseUrl}/login`, user);
   }
 }
